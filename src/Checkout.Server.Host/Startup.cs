f497d5763e61fe9d203bc17265f5d9217e6e295c
Stefano Gajon <stefano.gajon@deltatre.com>
2017-07-20 02:05:48 +0200

    Implemented Dummy Oauth2 with JWT

--- src/Checkout.Server.Host/Startup.cs
+++ src/Checkout.Server.Host/Startup.cs
@@ -1,9 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net.Http.Formatting;
 using System.Web.Http;
-using System.Web.Mvc;
+using Checkout.Server.Api.Filters;
+using Checkout.Server.Host.OAuth;
 using Checkout.Server.Ioc.Registrars;
 using FluentValidation.WebApi;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 using Owin;
 
@@ -15,6 +22,7 @@ namespace Checkout.Server.Host
         {
             var config = new HttpConfiguration();
 
+            ConfigureOAuth(app);
             ConfigurationHttp(config);
 
             var container = new WindsorContainerConfigurator().Configure(config);
@@ -34,6 +42,39 @@ namespace Checkout.Server.Host
 
             var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
             jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Filters.Add(new ValidateModelStateFilter());
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            // OAuth 2.0 Bearer Access Token Generation
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions()
+            {
+                //For Dev enviroment only (on production should be AllowInsecureHttp = false)
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/oauth/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat("http://localhost:26603")
+            });
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            var issuer = "http://localhost:26603";
+            var audience = "099153c2625149bc8ecb3e85e03f0022";
+            var secret = TextEncodings.Base64Url.Decode("IxrAjDoa2FqElO7IhrSrUJELhUckePEPVpaePlS_Xaw");
+
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audience },
+
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                    }
+                });
         }
     }
 
